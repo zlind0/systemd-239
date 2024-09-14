systemd/systemd

units: allow and use SuccessAction=exit-force in system systemd-exit.service
C.f. 287419c119ef961db487a281162ab037eba70c61: 'systemctl exit 42' can be
used to set an exit value and pulls in exit.target, which pulls in systemd-exit.service,
which calls org.fdo.Manager.Exit, which calls method_exit(), which sets the objective
to MANAGER_EXIT. Allow the same to happen through SuccessAction=exit.

v2: update for 'exit' and 'exit-force'

src/core/emergency-action.c
@@ -10,6 +10,7 @@
 #include "special.h"
 #include "string-table.h"
 #include "terminal-util.h"
+#include "virt.h"
 
 static void log_and_status(Manager *m, const char *message, const char *reason) {
         log_warning("%s: %s", message, reason);
@@ -71,12 +72,14 @@ int emergency_action(
                 break;
 
         case EMERGENCY_ACTION_EXIT:
-                assert(MANAGER_IS_USER(m));
+                if (MANAGER_IS_USER(m) || detect_container() > 0) {
+                        log_and_status(m, "Exiting", reason);
+                        (void) manager_add_job_by_name_and_warn(m, JOB_START, SPECIAL_EXIT_TARGET, JOB_REPLACE_IRREVERSIBLY, NULL);
+                        break;
+                }
 
-                log_and_status(m, "Exiting", reason);
-
-                (void) manager_add_job_by_name_and_warn(m, JOB_START, SPECIAL_EXIT_TARGET, JOB_REPLACE_IRREVERSIBLY, NULL);
-                break;
+                log_notice("Doing \"poweroff\" action instead of an \"exit\" emergency action.");
+                _fallthrough_;
 
         case EMERGENCY_ACTION_POWEROFF:
                 log_and_status(m, "Powering off", reason);
@@ -84,11 +87,14 @@ int emergency_action(
                 break;
 
         case EMERGENCY_ACTION_EXIT_FORCE:
-                assert(MANAGER_IS_USER(m));
+                if (MANAGER_IS_USER(m) || detect_container() > 0) {
+                        log_and_status(m, "Exiting immediately", reason);
+                        m->objective = MANAGER_EXIT;
+                        break;
+                }
 
-                log_and_status(m, "Exiting immediately", reason);
-                m->objective = MANAGER_EXIT;
-                break;
+                log_notice("Doing \"poweroff-force\" action instead of an \"exit-force\" emergency action.");
+                _fallthrough_;
 
         case EMERGENCY_ACTION_POWEROFF_FORCE:
                 log_and_status(m, "Forcibly powering off", reason);
@@ -135,8 +141,7 @@ int parse_emergency_action(
         if (x < 0)
                 return -EINVAL;
 
-        if ((system && x >= _EMERGENCY_ACTION_FIRST_USER_ACTION) ||
-            (!system && x != EMERGENCY_ACTION_NONE && x < _EMERGENCY_ACTION_FIRST_USER_ACTION))
+        if (!system && x != EMERGENCY_ACTION_NONE && x < _EMERGENCY_ACTION_FIRST_USER_ACTION)
                 return -EOPNOTSUPP;
 
         *ret = x;

zlind0/systemd-239

(cherry picked from commit a400bd8c2a6285576edf8e2147e1d17aab129501)
Related: #1860899

src/core/emergency-action.c
@@ -13,6 +13,7 @@
 #include "special.h"
 #include "string-table.h"
 #include "terminal-util.h"
+#include "virt.h"
 
 static void log_and_status(Manager *m, const char *message, const char *reason) {
         log_warning("%s: %s", message, reason);
@@ -73,12 +74,14 @@ int emergency_action(
                 break;
 
         case EMERGENCY_ACTION_EXIT:
-                assert(MANAGER_IS_USER(m));
+                if (MANAGER_IS_USER(m) || detect_container() > 0) {
+                        log_and_status(m, "Exiting", reason);
+                        (void) manager_add_job_by_name_and_warn(m, JOB_START, SPECIAL_EXIT_TARGET, JOB_REPLACE_IRREVERSIBLY, NULL, NULL);
+                        break;
+                }
 
-                log_and_status(m, "Exiting", reason);
-
-                (void) manager_add_job_by_name_and_warn(m, JOB_START, SPECIAL_EXIT_TARGET, JOB_REPLACE_IRREVERSIBLY, NULL, NULL);
-                break;
+                log_notice("Doing \"poweroff\" action instead of an \"exit\" emergency action.");
+                _fallthrough_;
 
         case EMERGENCY_ACTION_POWEROFF:
                 log_and_status(m, "Powering off", reason);
@@ -86,11 +89,14 @@ int emergency_action(
                 break;
 
         case EMERGENCY_ACTION_EXIT_FORCE:
-                assert(MANAGER_IS_USER(m));
+                if (MANAGER_IS_USER(m) || detect_container() > 0) {
+                        log_and_status(m, "Exiting immediately", reason);
+                        m->exit_code = MANAGER_EXIT;
+                        break;
+                }
 
-                log_and_status(m, "Exiting immediately", reason);
-                m->exit_code = MANAGER_EXIT;
-                break;
+                log_notice("Doing \"poweroff-force\" action instead of an \"exit-force\" emergency action.");
+                _fallthrough_;
 
         case EMERGENCY_ACTION_POWEROFF_FORCE:
                 log_and_status(m, "Forcibly powering off", reason);
@@ -137,8 +143,7 @@ int parse_emergency_action(
         if (x < 0)
                 return -EINVAL;
 
-        if ((system && x >= _EMERGENCY_ACTION_FIRST_USER_ACTION) ||
-            (!system && x != EMERGENCY_ACTION_NONE && x < _EMERGENCY_ACTION_FIRST_USER_ACTION))
+        if (!system && x != EMERGENCY_ACTION_NONE && x < _EMERGENCY_ACTION_FIRST_USER_ACTION)
                 return -EOPNOTSUPP;
 
         *ret = x;