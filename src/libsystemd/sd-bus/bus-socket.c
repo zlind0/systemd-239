systemd/systemd

sd-bus: handle -EINTR return from bus_poll()
In sd_bus_wait(), let's convert EINTR to a return code of 0, thus asking
the caller do loop again and enter sd_bus_process() again (which will
not find any queued events). This way we'll not return an error on
something that isn't really an error. This should typically make sure
things are properly handled by the caller, magically, without eating up
the event entirely, and still giving the caller time to run some code if
they want.

src/libsystemd/sd-bus/bus-socket.c
@@ -1308,8 +1308,11 @@ int bus_socket_process_opening(sd_bus *b) {
         assert(b->state == BUS_OPENING);
 
         events = fd_wait_for_event(b->output_fd, POLLOUT, 0);
-        if (events < 0)
+        if (events < 0) {
+                if (ERRNO_IS_TRANSIENT(events))
+                        return 0;
                 return events;
+        }
         if (!(events & (POLLOUT|POLLERR|POLLHUP)))
                 return 0;
 

zlind0/systemd-239

(cherry picked from commit 3022916b4d2483452c3ddbbac9ee7c4372b1cb46)
Resolves: #2172846

src/libsystemd/sd-bus/bus-socket.c
@@ -1266,8 +1266,11 @@ int bus_socket_process_opening(sd_bus *b) {
         assert(b->state == BUS_OPENING);
 
         r = poll(&p, 1, 0);
-        if (r < 0)
+        if (r < 0) {
+                if (ERRNO_IS_TRANSIENT(errno))
+                        return 0;
                 return -errno;
+        }
 
         if (!(p.revents & (POLLOUT|POLLERR|POLLHUP)))
                 return 0;